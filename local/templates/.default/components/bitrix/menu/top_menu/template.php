5ab5cc7b934bcee62b07ce7fb6d6a68f4855a05e
Admin <git@webgk.ru>
2018-07-17 11:08:02 +0300

    SUP-8149: update top menu

--- local/templates/.default/components/bitrix/menu/top_menu/template.php
+++ local/templates/.default/components/bitrix/menu/top_menu/template.php
@@ -1,13 +1,14 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-
 <?
-//test_dump($arResult);
-
-
+//arshow($arResult);
 foreach($arResult as $arItem) {
 	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1)
 		continue;
+        
+        $style= "";
+        if ($arItem["PARAMS"]["color"]) {
+            $style = "style='color: ". $arItem["PARAMS"]["color"]." !important'";    
+        }
 ?>
-	<li><a class="topMenuLink<?if($arItem['SELECTED']){?> topMenuLink_selected<?}?>" href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+	<li><a <?=$style?> class="topMenuLink<?if($arItem['SELECTED']){?> topMenuLink_selected<?}?>" href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
 <?}?>
-<li><a class="topMenuLink" href="/actions/freedigitalbooks/" target="_blank">Бесплатные электронные книги</a></li>
