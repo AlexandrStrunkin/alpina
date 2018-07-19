7386fe3919476cd9025aff43705094afc4a11473
Admin <git@webgk.ru>
2018-07-19 15:01:27 +0300

    SUP-7996: edit setka

---
+++ blog/setka_editor.php
@@ -0,0 +1,67 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");?>
+<?
+global $USER;
+if($USER->IsAdmin()){
+            // Ваш ключ доступа к API (из Личного Кабинета Юнисендер)
+        $api_key = KEY_SETKA;
+        
+        // Создаём POST-запрос
+        $POST = array (
+          'token' => $api_key,
+        );
+
+        // Устанавливаем соединение
+          if( $curl = curl_init() ) {
+            curl_setopt($curl, CURLOPT_URL, 'https://editor.setka.io/api/v1/custom/builds/current?token='.$api_key);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+            $result = curl_exec($curl);
+          }
+
+        if ($result) {
+          // Раскодируем ответ API-сервера
+          $jsonObj = object_in_array(json_decode($result));
+        }         
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+    <!-- 1. Подключаем основной файл редактора -->
+    <script type="text/javascript" src="<?=$jsonObj["plugins"][0]["url"]?>" async></script>
+    
+    <!-- 2. Подключаем стили интерфейса редактора -->
+    <script type="text/javascript" src="<?//=$jsonObj["theme_files"][1]["url"]?>" async></script>
+    <script type="text/javascript" src="<?=$jsonObj["content_editor_files"][1]["url"]?>" async></script>
+    <link rel="stylesheet" href="<?=$jsonObj["theme_files"][0]["url"]?>">
+    <link rel="stylesheet" href="<?=$jsonObj["content_editor_files"][0]["url"]?>">
+    <script>
+         $(document).ready(function () {
+
+
+            fetch('https://www.alpinabook.ru/js/setka/setka.json').then(response => response.json()).then(response => {
+
+                const config = response.config;
+                const assets = response.assets;
+                config.headerTopOffset = 20;    // ерхний отступ для верхней панели редактора.
+                config.footerBottomOffset = 20; // нижний отступ для нижней панели редактора.
+
+            // Передаем публичный токен (public_token) компании
+            // (он нужен для отправки запросов из редактора в API editor.setka.io)
+            config.public_token = '<?=KEY_SETKA?>';
+            config.token = '<?=KEY_SETKA?>';
+
+             // Стартуем редактор 
+            SetkaEditor.start(config, assets); 
+            }) .catch(ex => alert(ex));
+         }); 
+    </script>
+  </head>
+  <body>
+    <!-- 4. Размещаем на странице контейнер для редактора -->
+    <div class="stk-editor" id="setka-editor"></div>
+   </body>
+</html>
+<?}?>
+
+ 
